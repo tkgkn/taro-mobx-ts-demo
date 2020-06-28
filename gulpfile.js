d0fc97a05aaf6f43df6202a3d505a0c194a3afe6
c690554125 <690554125@qq.com>
2020-06-28 21:53:24 +0800

    测试预览自定义命令成功

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const gulp = require('gulp');
 const uglify = require('gulp-uglify');
-const cleanCss = require('gulp-clean-css');
-const htmlMin = require('gulp-htmlmin');
+const cssnano = require('gulp-cssnano');
 const htmlClean = require('gulp-htmlclean');
 const del = require('del');
 
@@ -21,25 +20,21 @@ function minifyJS() {
 function minifyCSS() {
   return gulp
     .src('dist/**/*.wxss')
-    .pipe(cleanCss())
+    .pipe(cssnano())
     .pipe(gulp.dest('./build'));
 }
 
 function minifyHTML() {
   return gulp
     .src('dist/**/*.wxml')
-    .pipe(
-      htmlMin({
-        removeComments: true,
-        collapseWhitespace: true
-      })
-    )
     .pipe(htmlClean())
     .pipe(gulp.dest('./build'));
 }
 
 function copyOther() {
-  return gulp.src('dist/**/*.map').pipe(gulp.dest('./build'));
+  return gulp
+    .src(['dist/**/*.map', 'dist/**/*.json', 'dist/**/*.wxs'])
+    .pipe(gulp.dest('./build'));
 }
 
 function cleanBuild() {
@@ -50,12 +45,17 @@ function cleanDist() {
   return del(['dist/**/*']);
 }
 
+function carry() {
+  return gulp.src('build/**/*').pipe(gulp.dest('dist'));
+}
+
 gulp.task(
   'build',
   gulp.series(
     cleanBuild,
     gulp.parallel(minifyJS, minifyCSS, minifyHTML, copyOther),
-    cleanDist
+    cleanDist,
+    carry
   )
 );
 
